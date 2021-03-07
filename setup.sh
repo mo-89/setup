2c3a4b15ad8b9bef7b9824e7602b32f2aa222415
masa <masa@masanoMacBook-Pro.local>
2021-03-07 11:21:58 +0900

    add row

--- setup.sh
+++ setup.sh
@@ -6,9 +6,15 @@ brew install alfred
 brew install visual-studio-code
 brew install google-japanese-ime
 brew install flux
+brew install mas 
+brew install slack
+brew install discord
+brew install zoom
+#brew install 
+#brew install 
 #brew install 
 #brew install 
 
 
 
-
+mas install 539883307 # LINE
