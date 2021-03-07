bbc3c1044c4fb7951f6bd18da5a1480bc7b95696
masa <masa@masanoMacBook-Pro.local>
2021-03-07 10:45:56 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!bin/sh
+
+brew install firefox
+brew install iterm2
+brew install alfred
+brew install visual-studio-code
+brew install google-japanese-ime
+brew install flux
+#brew install 
+#brew install 
+
+
+
+
